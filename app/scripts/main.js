99529454e910d17e7eae69c80d7b6470caac8a75
ItsJonQ <itsjonq@gmail.com>
2015-01-20 23:36:12 -0500

    Setup project with yeoman backbone

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global TriviaDb, $*/
+
+
+window.TriviaDb = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    TriviaDb.init();
+});
